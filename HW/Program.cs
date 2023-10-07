6a4064f76e3df2ebc362630342630ee782546bf4
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2023-10-06 23:53:57 -0700

    Add files via upload

---
+++ HW/Program.cs
@@ -0,0 +1,10 @@
+﻿
+double radius, pie;
+
+Console.Write("Enter a radius: ");
+
+radius = double.Parse(Console.ReadLine());
+
+pie = Math.PI;
+
+Console.WriteLine("A circle with radius " + radius + " has an area of " + (pie * (radius * radius)) + ".");
